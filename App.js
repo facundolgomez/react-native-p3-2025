7cdd61f22d578a052aa4cd7e93643f8974407b33
Facundo Gomez <facundolgomez87@gmail.com>
2025-10-14 23:36:35 -0300

    agrego nuevo estado y funciones

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
-
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+} from "react-native";
+import { useState } from "react";
 export default function App() {
+  const [text, setText] = useState("");
+  const [readings, setReadings] = useState([]);
+  const handleReadingInput = (enteredText) => {
+    setText(enteredText);
+  };
+  const handleAddReading = () => {
+    setReadings((prevReadings) => [...prevReadings, text]);
+  };
+
+  const mappedReadings = readings.map((reading) => {
+    return (
+      <Text style={styles.readingItem} key={reading}>
+        {" "}
+        {reading}
+      </Text>
+    );
+  });
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
           placeholder="Ingrese el nombre del libro "
+          onChangeText={handleReadingInput}
         />
         <View style={styles.buttonInput}>
-          <Button title="Agregar lectura" />
+          <Button title="Agregar lectura" onPress={handleAddReading} />
         </View>
       </View>
       <View>
         <Text>Lista de lecturas...</Text>
+      </View>
+      <View>
+        <ScrollView>{mappedReadings}</ScrollView>
       </View>
     </View>
   );
@@ -37,5 +65,12 @@ const styles = StyleSheet.create({
   buttonInput: {
     width: "50%",
     alignSelf: "flex-end",
+  },
+  readingItem: {
+    margin: 12,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "green",
+    color: "white",
   },
 });
